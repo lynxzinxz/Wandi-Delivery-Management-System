a8e7ee75eeba03ffb96535b1a9f5b39102af8c0a
lynxzin <marvilynfrias11@gmail.com>
2026-05-07 04:34:10 +0800

    Initial: main menu and viewOrders WIP setup

--- src/wandideliverymanagementsystem/WandiDeliveryManagementSystem.java
+++ src/wandideliverymanagementsystem/WandiDeliveryManagementSystem.java
@@ -1,21 +1,72 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package wandideliverymanagementsystem;
 
-/**
- *
- * @author Lenovo
- */
-public class WandiDeliveryManagementSystem {
+import java.util.*;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class WandiDeliveryManagementSystem {
     
+    
+
+    // showMenu method
+    public static void ShowMenu() {
+        System.out.println("====================================================\n"
+                + "            Wandi Delivery Tracking System\n"
+                + "====================================================");
+
+        System.out.println("  [1]  Add Order             [4]  View Orders \n"
+                + "  [2]  Delete Order          [5]  Update Status\n"
+                + "  [3]  Search Order          [6]  Filter by Status\n"
+                + "        \t   [7]  Exit\n"
+                + "-----------------------------------------------------");
+    }
+
+    // viewOrders method
+    public static void ViewOrders() {
+        System.out.println();
+        System.out.println("====================================================\n"
+                + "         WANDI TRACKING SYSTEM — ALL ORDERS\n"
+                + "====================================================");
+        System.out.println();
+
+//        for 
+//        System.out.println("[ Preparing ]                              3 orders ");
+//        System.out.println("-----------------------------------------------------");
+//        System.out.println("  #1  SPX100267       Juan dela Cruz");
+//        System.out.println("-----------------------------------------------------");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        ShowMenu();
+        System.out.print("Select option: ");
+        int option = sc.nextInt();
+
+        switch (option) {
+//            case 1:
+//                AddOrder();
+//                break;
+//
+//            case 2:
+//                DeleteOrder();
+//                break;
+
+//            case 3:
+//                SearchOrder();
+//                break;
+
+            case 4:
+                ViewOrders();
+                break;
+
+//            case 5:
+//                UpdateStatus;
+//                break;
+//               
+//            case 6:
+//                FilterByStatus();
+//                break;
+        }
+
+    }
+
 }
